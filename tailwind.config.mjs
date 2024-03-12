a8c9ea6cab7f8823d87f211e3260097ca2ba0db6
komosyu4649 <komosyu4649@gmail.com>
2024-03-12 20:53:51 +0900

    Add Contact component to Layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -6,7 +9,24 @@ export default {
 			colors: {
 				default: "#242424",
 				key: "#5A93A4",
-			}
+				gray: "#AEAEAE",
+				white: "#FCFCFC"
+			},
+			fontSize: {
+				defaultText: [
+					"1.6rem",
+					{
+						fontWeight: "DemiLight",
+						lineHeight: "2",
+					}
+				],
+
+			},
+			fontFamily: {
+				"sans": ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+				"mincho": '"BIZ UDPMincho"'
+				,
+			},
 		},
 	},
 	plugins: [],
