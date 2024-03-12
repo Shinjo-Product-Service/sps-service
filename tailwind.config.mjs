b1ecd291cc9c4e550f3c82031ea1698ade3c6381
komosyu4649 <komosyu4649@gmail.com>
2024-03-12 13:18:48 +0900

    Update Header component and global styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				default: "#242424",
+				key: "#5A93A4",
+			}
+		},
 	},
 	plugins: [],
 }
