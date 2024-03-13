2d493cb208d52c2dd1baf8269282567b7e1940ec
komosyu4649 <komosyu4649@gmail.com>
2024-03-13 21:58:06 +0900

    コードの変更を記述するための短く簡潔なコミットメッセージを提案します。***
    ***tsconfig.json: ファイルの末尾に改行を追加***
    ***package.json: devDependenciesにprettierとprettier-plugin-astroを追加***
    ***README.md: Discordサーバーへのリンクを追加***
    ***astro.config.mjs: インデントを修正***
    ***global.css: インデントを修正***
    ***Contact.astro: ボタンのテキストサイズを変更***
    ***Link.astro: リンクのスタイルを微調整***
    ***Header.astro: メニューとボタンのテキストサイズを変更***
    ***Footer.astro: テキストサイズを微調整***
    ***arrow.svg: アイコンのサイズを変更***
    ***tailwind.config.mjs: インデントを修正***
    
    ***以上がコードの変更内容です。

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,45 +1,44 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			width: {
-				defaultLayout: '128rem',
-			},
-			colors: {
-				default: "#242424",
-				key: "#5A93A4",
-				gray: "#AEAEAE",
-				white: "#FCFCFC",
-				lineGray: "#D3D3D3"
-			},
-			fontSize: {
-				defaultText: [
-					"1.6rem",
-					{
-						fontWeight: "DemiLight",
-						lineHeight: "2",
-					}
-				],
-				sectionEnTitle: [
-					"1.8rem",
-					{
-						fontWeight: "Regular",
-						fontFamily: "urbanist",
-						lineHeight: "1.4",
-					}
-				],
-
-			},
-			fontFamily: {
-				"sans": ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
-				"mincho": '"BIZ UDPMincho"',
-				"urbanist": '"Urbanist"',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      width: {
+        defaultLayout: "128rem",
+      },
+      colors: {
+        default: "#242424",
+        key: "#5A93A4",
+        gray: "#AEAEAE",
+        white: "#FCFCFC",
+        lineGray: "#D3D3D3",
+      },
+      fontSize: {
+        defaultText: [
+          "1.6rem",
+          {
+            fontWeight: "DemiLight",
+            lineHeight: "2",
+          },
+        ],
+        sectionEnTitle: [
+          "1.8rem",
+          {
+            fontWeight: "Regular",
+            fontFamily: "urbanist",
+            lineHeight: "1.4",
+          },
+        ],
+      },
+      fontFamily: {
+        sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
+        mincho: '"BIZ UDPMincho"',
+        urbanist: '"Urbanist"',
+      },
+    },
+  },
+  plugins: [],
+};
