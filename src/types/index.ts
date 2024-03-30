dddfcb4957b19843da0f60e280cdd691180db2ea
komosyu4649 <komosyu4649@gmail.com>
2024-03-30 11:50:55 +0900

    事例追加

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,72 @@ export type NewsItemType = {
   label: string;
   content: string;
   _sys: {
-    createdAt: string;
-    updatedAt: string;
+    createdAt: Date;
+    updatedAt: Date;
   };
+};
+
+type articleInfoType = {
+  title: string;
+  image: {
+    _id: string;
+    altText: string;
+    description: string;
+    fileName: string;
+    fileSize: number;
+    fileType: string;
+    height: number;
+    metadata: {};
+    src: string;
+    title: string;
+    width: number;
+  };
+  description: string;
+};
+
+export type WorksItemType = {
+  _id: string;
+  _sys: {
+    raw: [Object];
+    customOrder: number;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+  title: string;
+  thumbnail: {
+    _id: string;
+    altText: string;
+    description: string;
+    fileName: string;
+    fileSize: number;
+    fileType: string;
+    height: number;
+    metadata: {};
+    src: string;
+    title: string;
+    width: number;
+  };
+  client: {
+    name: string;
+    logo: {
+      _id: string;
+      altText: string;
+      description: string;
+      fileName: string;
+      fileSize: number;
+      fileType: string;
+      height: number;
+      metadata: {};
+      src: string;
+      title: string;
+      width: number;
+    };
+  };
+  description: string;
+  industry: string;
+  numberOfEmployees: string;
+  issue: string;
+  beforeIntroduction: articleInfoType;
+  selectedReason: articleInfoType;
+  afterIntroduction: articleInfoType;
 };
